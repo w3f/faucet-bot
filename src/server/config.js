d3949a4000d342566d3fc3a333a649d20393dc22
Irsal Alsanea <irsal@oak.tech>
2022-03-11 10:49:26 -0800

    Change endpoint to oaks own

--- src/server/config.js
+++ src/server/config.js
@@ -6,7 +6,7 @@ if (!process.env.FAUCET_MNEMONIC) {
 module.exports = {
   mnemonic: process.env.FAUCET_MNEMONIC,
   polkadot: {
-    endpoint: process.env.CHAIN_WS_ENDPOINT || 'wss://neumann.api.onfinality.io/public-ws',
+    endpoint: process.env.CHAIN_WS_ENDPOINT || 'wss://rpc.testnet.oak.tech',
     ss58Preifx: 42,
   },
   port: process.env.PORT || 5555,
